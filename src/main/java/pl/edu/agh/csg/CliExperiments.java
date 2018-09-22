f6d92b2fc354c024ef8a3a47979cbc4ab1cbea43
Pawel Koperek <p.koperek@eyeo.com>
2018-09-22 14:38:39 +0200

    profile rendering method, result: no concerns here (~ 1.5E-4s)

--- src/main/java/pl/edu/agh/csg/CliExperiments.java
+++ src/main/java/pl/edu/agh/csg/CliExperiments.java
@@ -12,14 +12,21 @@ import java.util.Arrays;
 class CliExperiments {
     public static void main(String[] args) {
         try {
-            SimulationEnvironment simulationEnvironment = new SimulationEnvironment("KTH-SP2-1996-2.1-cln.swf");
+            SimulationEnvironment simulationEnvironment = new SimulationEnvironment("KTH-SP2-1996-2.1-cln_50.swf");
             simulationEnvironment.reset();
             double totalReward = 0.0;
-            while(true) {
+            while (true) {
                 SimulationStepResult stepResult = simulationEnvironment.step(0);
                 totalReward += stepResult.getReward();
+
+                final long start = System.nanoTime();
+                double[][] env = simulationEnvironment.render();
+                final long stop = System.nanoTime();
+                System.out.println("Render time: " + ((stop - start) / 1000000000.0) + "s");
+                System.out.println("Render size: " + env.length + " x " + env[0].length);
+
                 System.out.println("Observations: " + Arrays.toString(stepResult.getObs()) + " reward: " + stepResult.getReward());
-                if(stepResult.isDone()) {
+                if (stepResult.isDone()) {
                     System.out.println(">>> SIMULATION FINISHED <<<");
                     break;
                 }
