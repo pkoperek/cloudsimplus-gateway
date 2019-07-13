441f2719a1422956eaf53bf82ad5ce42dfe49a42
Pawel Koperek <pkoperek@gmail.com>
2019-07-14 00:17:16 +0200

    Integration tests

---
+++ src/test/java/pl/edu/agh/csg/IntegrationTest.java
@@ -0,0 +1,64 @@
+package pl.edu.agh.csg;
+
+import static org.junit.Assert.*;
+
+import com.google.gson.Gson;
+import org.cloudbus.cloudsim.cloudlets.Cloudlet;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class IntegrationTest {
+
+    final MultiSimulationEnvironment multiSimulationEnvironment = new MultiSimulationEnvironment();
+    final Gson gson = new Gson();
+
+    @Test
+    public void testPing() {
+        final long ping = multiSimulationEnvironment.ping();
+
+        assertEquals(31415L, ping);
+    }
+
+    @Test
+    public void testSimulationSingleStep() {
+        CloudletDescriptor cloudletDescriptor = new CloudletDescriptor(1, 10, 10000, 4);
+
+        List<CloudletDescriptor> jobs = Arrays.asList(cloudletDescriptor);
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put(SimulationFactory.SOURCE_OF_JOBS_PARAMS_JOBS, gson.toJson(jobs));
+
+        final String simulationId = multiSimulationEnvironment.createSimulation(parameters);
+
+        multiSimulationEnvironment.reset(simulationId);
+        multiSimulationEnvironment.step(simulationId, 0);
+        multiSimulationEnvironment.close(simulationId);
+    }
+
+    @Test
+    public void testSimulationWithSingleJob() {
+        // Job should start after 10 iterations and last for next 10
+        CloudletDescriptor cloudletDescriptor = new CloudletDescriptor(1, 10, 100000, 4);
+
+        List<CloudletDescriptor> jobs = Arrays.asList(cloudletDescriptor);
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put(SimulationFactory.SOURCE_OF_JOBS_PARAMS_JOBS, gson.toJson(jobs));
+
+        final String simulationId = multiSimulationEnvironment.createSimulation(parameters);
+
+        multiSimulationEnvironment.reset(simulationId);
+        int stepsExecuted = 1;
+        SimulationStepResult step = multiSimulationEnvironment.step(simulationId, 0);
+        while(!step.isDone()) {
+            System.out.println("Executing step: " + stepsExecuted);
+            step = multiSimulationEnvironment.step(simulationId, 0);
+            stepsExecuted++;
+        }
+        multiSimulationEnvironment.close(simulationId);
+
+        assertEquals(27, stepsExecuted);
+    }
+}
