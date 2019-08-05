bc3b69d8aaaa6515d1a916100b6cdffa1775e815
Pawel Koperek <pkoperek@gmail.com>
2019-08-06 00:24:40 +0200

    Log how many jobs were deserialized

--- src/main/java/pl/edu/agh/csg/SimulationFactory.java
+++ src/main/java/pl/edu/agh/csg/SimulationFactory.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.apache.commons.lang3.NotImplementedException;
 import org.cloudbus.cloudsim.cloudlets.Cloudlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -12,6 +14,7 @@ import java.util.Map;
 
 public class SimulationFactory {
 
+    private static final Logger logger = LoggerFactory.getLogger(SimulationFactory.class.getName());
     private static final Type cloudletDescriptors = new TypeToken<List<CloudletDescriptor>>() {}.getType();
 
     public static final String INITIAL_VM_COUNT = "INITIAL_VM_COUNT";
@@ -65,6 +68,8 @@ public class SimulationFactory {
         for(CloudletDescriptor cloudletDescriptor : deserialized) {
             retVal.add(cloudletDescriptor.toCloudlet());
         }
+
+        logger.info("Deserialized " + retVal.size() + " jobs");
 
         return retVal;
     }
